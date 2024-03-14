5c4b0c1fb1221d57baa73f1592f45498f3f25a7f
Mariana Cardoso <marianacardoso.tds2.senai@gmail.com>
2024-03-14 10:51:39 -0300

    Método 01: .concat()

---
+++ ExercicioArray.js
@@ -0,0 +1,16 @@
+//Atividade JS - Arrays e Github
+
+//Declarar os Arrays
+let array1 = ['a','b', 'c', 'd'];
+let array2 = ['x', 'y', 'z'];
+
+//Método 01: .concat()
+//O método concat() é usado para combinar dois ou mais arrays em um único array.
+
+//sintaxe 
+//array1.concat(array2)
+
+//Teste prático:
+let array3 = array1.concat(array2);
+
+console.log(array3);
