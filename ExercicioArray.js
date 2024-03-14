5f3b928fd46d5609606235fa7a273a6a7e56b2da
Mariana Cardoso <marianacardoso.tds2.senai@gmail.com>
2024-03-14 11:09:29 -0300

    Metodo 02: .join()

--- ExercicioArray.js
+++ ExercicioArray.js
@@ -1,7 +1,7 @@
 //Atividade JS - Arrays e Github
 
 //Declarar os Arrays
-let array1 = ['a','b', 'c', 'd'];
+let array1 = ['a', 'b', 'c', 'd'];
 let array2 = ['x', 'y', 'z'];
 
 //Método 01: .concat()
@@ -10,7 +10,28 @@ let array2 = ['x', 'y', 'z'];
 //sintaxe 
 //array1.concat(array2)
 
-//Teste prático:
+//Testes práticos:
 let array3 = array1.concat(array2);
-
 console.log(array3);
+
+let array4 = array1.concat(array2, array3);
+console.log(array4);
+
+
+//Metodo 02: .join()
+//O método join() é usado para criar uma string a partir dos elementos de um array.
+//O método join() junta os elementos do array em uma única string, separando cada elemento com um separador especificado.
+
+//Sintaxe:
+//join()
+//join(separador)
+
+//Testes práticos:
+let equipe = ['Júlia', 'Vinicius', 'Fernanda', 'Rayssa', 'Mariana']
+
+//Mostrar os elementos da seguinte forma: Júlia,Vinicius,Fernanada,Rayssa,Mariana
+console.log(equipe.join());
+
+//Separar os elementos da seguinte forma: Júlia-Vinicius-Fernanada-Rayssa-Mariana
+console.log(equipe.join('-'))
+
